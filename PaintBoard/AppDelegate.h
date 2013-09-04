c2b5709168cdb6245c7b6d55749941567897f616
ErLi <lier0715@bu.edu>
2013-09-04 12:35:27 -0400

    Initial Commit

---
+++ PaintBoard/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PaintBoard
+//
+//  Created by smart_parking on 9/4/13.
+//  Copyright (c) 2013 codes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PaintBoard/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PaintBoard
+//
+//  Created by smart_parking on 9/4/13.
+//  Copyright (c) 2013 codes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
